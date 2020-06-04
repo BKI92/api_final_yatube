8a971891a07b902e10e300a643ffec7bbca83eb8
Konstantin <balashov-92@mail.ru>
2020-06-05 00:22:02 +0300

    add models Post, Group, Follower

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Post, Group, Follow
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "pub_date", "author", "group")
+    search_fields = ("text",)
+    list_filter = ("pub_date",)
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug")
+    search_fields = ("title",)
+    list_filter = ("title",)
+    empty_value_display = '-пусто-'
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("user", "following")
+    list_filter = ("user",)
+    empty_value_display = '-пусто-'
+
+
+# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Follow, FollowAdmin)
+
+from django.contrib import admin
 
 # Register your models here.
